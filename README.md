9f6b31709c650a697d123c33248fd37c367d2e25
Anita Mehrotra <anita_mehrotra@yahoo.com>
2020-04-16 20:39:21 -0400

    contents reflected in README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# data-science-checklist
-Commonly used functions and checks for transformations, EDA and modeling.
+# README.md
+
+Commonly-used functions and checks for initial exploratory analysis of data transformations, EDA and modeling.
+
+> Contents
+
+* `analyze`: summary statistics, coverage, and outliers.
+* `feature_engineering`: one-hot encoding and handling datetimestamps.
+* `model_assumptions`: generating collinearity plots.
--- analyze.py
+++ analyze.py
@@ -4,9 +4,6 @@ import pandas as pd
 import scipy.stats as ss
 import scipy.special as sp
 
-from datetime import datetime, timedelta
-from pytz import timezone
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -87,55 +84,4 @@ def find_outliers(col):
     print('% outliers:', percent_outliers)
     
     return outliers_indices, len(outliers_indices), percent_outliers
-
-
-## Handling timestamps.
-
-def convert_string_to_hour(s):
-    '''
-    :input string / float s: timestamp / NaN
-    :returns numeric: hour for string of timestamp; otherwise NaN
-    '''
-    if isinstance(s, float):
-        return s
-    else:
-        return int(s[11:13])
-
-
-def convert_string_to_datetime(s, timezone_str):
-    '''
-    :input string / float s: timestamp / NaN
-    :input string timezone: e.g. 'US/Pacific'
-    :returns datetime:
-    '''
-    if isinstance(s, float):
-        return s
-    else:
-        datetime_object = datetime.strptime(s[:19], '%Y-%m-%d %H:%M:%S')
-        return datetime_object.replace(tzinfo=timezone(timezone_str))
-
-
-def convert_datetime_to_number(d, timezone_str):
-    '''
-    Ideally input should be a datetime object. 
-    if not, return it. that's likely because it's a nan.
-    :input datetime obj d: includes timezone
-    :input string timezone: e.g. 'US/Pacific'
-    :returns timedelta in seconds:
-    '''
-    if isinstance(d, float):
-        return d
-    else:
-        special_date = datetime(1970, 1, 1, tzinfo=timezone(timezone_str))
-        return (d - special_date).total_seconds()
-    
-def convert_timestamp_to_date(ts):
-    '''
-    Check timezone.
-    :input int ts: timestamp
-    '''
-    if isinstance(ts, float):
-        return ts
-    else:
-        return datetime.fromtimestamp(ex_d_int)
 
