c4d1c5511002f1c47670e884dc028e93b7aba5a1
Anita Mehrotra <anita_mehrotra@yahoo.com>
2020-04-17 20:13:42 -0400

    beginning to rewrite docstrings; also generalized find_outliers()

--- analyze.py
+++ analyze.py
@@ -11,10 +11,10 @@ import seaborn as sns
 ## Basic initial stats.
 
 def get_basic_stats(df, datecol):
-    '''
+    """
     :input pandas.dataframe df:
     :input String datecol:
-    '''
+    """
     # number of rows
     num_rows = len(df)
     print('total # of rows: {} \n'.format(num_rows))
@@ -39,16 +39,16 @@ def get_basic_stats(df, datecol):
 ## Check coverage for the dataset.
 
 def report_overall_coverage(df):
-    '''
+    """
     :input pandas.dataframe df:
-    '''
+    """
     return (100*df.isna().sum().sum() / float(len(df)*len(df.columns)))
 
 
 def report_column_coverage(col):
-    '''
+    """
     :input numpy.ndarray col:
-    '''
+    """
     count_missing_vals = col.isna().sum()
     print('# missing values:', count_missing_vals)
     
@@ -64,6 +64,16 @@ def report_column_coverage(col):
 
 # Boxplot method. Why? Z-score biased. Other methods? DBScan, Isolation forests.
 def plot_outliers(col, title='', xlabel=''):
+    """Visualizes outliers with a boxplot.
+    
+    Args:
+        col (numpy.ndarray): Column of dataset.
+        title (String): Title to use for plot.
+        xlabel (String): Label to use for x-axis.
+    
+    Returns:
+        Boxplot of data in col.
+    """
     plt.figure(figsize=[16,4])
     sns.boxplot(col)
     plt.title(title)
@@ -71,12 +81,23 @@ def plot_outliers(col, title='', xlabel=''):
     plt.show()
     
     
-def find_outliers(col):
-    q1, q3 = np.percentile(col.dropna(), [25,75])
+def find_outliers(col, multiplier=1.5, lower_percentile=25, upper_percentile=75):
+    """Finds outliers using the interquartile range.
+    
+    Args:
+        col (numpy.ndarray): Column of dataset.
+        multiplier (int): Defaults to 1.5, industry-standard for outliers.
+        lower_percentile (int): Default is second quartile a.k. 25th.
+        upper_percentile (int): Default is third quartile a.k. 75th.
+    
+    Returns:
+        triple: The indices of the outliers, number of outliers, percent that are outliers.
+    """
+    q1, q3 = np.percentile(col.dropna(), [lower_percentile, upper_percentile])
     iqr = q3 - q1
 
-    lower_bound = q1 - (1.5*iqr) 
-    upper_bound = q3 + (1.5*iqr)
+    lower_bound = q1 - (multiplier*iqr) 
+    upper_bound = q3 + (multiplier*iqr)
 
     outliers_indices = np.where(col.dropna() > upper_bound)[0]  # drop missing values
     print('# outliers:', len(outliers_indices))    
